90fd1f919d8bb6baaf04ce3a48354451021f4632
isaacyeboah <iyeboah1@gmail.com>
2016-05-06 08:37:19 +0000

    Fixing ajax errors

--- js/custom.js
+++ js/custom.js
@@ -55,15 +55,15 @@ jQuery(document).ready(function(e) {
 			async: false,
 			dataType: "text"
 		})
-		.fail(function() {
-			form_el.find('.ajax-loader').hide();
-			if(transforms3d_supported) {
-				cube.find('.cube-slide-3').attr('title', '').html(form_el.data('ajax-fail-msg'));
-				cube.showBottom();
-			}
-			else
-				cube.find('.return-msg').attr('title', '').html(form_el.data('ajax-fail-msg')).fadeIn();
-		})
+		// .fail(function() {
+		// 	form_el.find('.ajax-loader').hide();
+		// 	if(transforms3d_supported) {
+		// 		cube.find('.cube-slide-3').attr('title', '').html(form_el.data('ajax-fail-msg'));
+		// 		cube.showBottom();
+		// 	}
+		// 	else
+		// 		cube.find('.return-msg').attr('title', '').html(form_el.data('ajax-fail-msg')).fadeIn();
+		// })
 		.done(function(message) {
 			form_el.find('.ajax-loader').hide();
 			if(message == "" || (typeof(message) == 'undefined')) {
